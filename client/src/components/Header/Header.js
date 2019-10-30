4905ae7ce74b806daf9eb005999a88feff2fec5a
Aaron Leopold <aaronleopold@ufl.edu>
2019-10-29 20:43:25 -0400

    basic navbar implemented

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -1,30 +1,34 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import './Header.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const Header = () => {
     return (
-        <div className='topnav'>
-            {/* Logo */}
-            <Link id="logo-link" to="/">
-                <img className="topnav-logo" src={ "/logo192.png" } alt="React logo" />
-            </Link>
-
-            {/* Page Links */}
-            <div className="topnav-right">
-                <Link className="topnav-link" to='/Register'>Sign in</Link>
-                <Link className="topnav-link" to='/projects'>Projects</Link>
-                <a className="topnav-link" target='_blank' rel="noopener noreferrer" href="https://www.facebook.com/groups/ufosc/events/?source=4&action_history=null&filter=calendar">
-                    Events
-                    <i className="fas fa-external-link-alt external-link" data-fa-transform="up-6"></i>
-                </a>
-                <a className="topnav-link" target='_blank' rel="noopener noreferrer" href="https://github.com/ufosc/club-resources">
-                    Resources
-                    <i className="fas fa-external-link-alt external-link" data-fa-transform="up-6 right-4"></i>
-                </a>
-                <Link className="topnav-link" to="/about">About</Link>
+        <nav class="navbar navbar-expand-md navbar-light bg-custom">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="#">logo here</a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarResponsive">
+                    <ul class="navbar-nav ml-auto">
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Home</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#terminals">About Us</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#skills">Buy</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#portfolio">Contact Us</a>
+                        </li>
+                    </ul>
+                </div>
             </div>
-        </div>
+        </nav>
     )
 }
 
