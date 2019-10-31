f5d8da8240e7a88c69d09cf64638940878587e45
Aaron Leopold <aaronleopold1221@gmail.com>
2019-10-31 12:01:07 -0400

    added footer placeholder

---
+++ client/src/components/Footer/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+//import { Link } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const Footer = () => {
+    return (
+        <div>
+            this will be the footer
+        </div>
+    )
+}
+
+export default Footer
--- client/src/views/Home/Home.js
+++ client/src/views/Home/Home.js
@@ -5,12 +5,17 @@ import './Home.css';
 function Home() {
     return (
         <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>
-                    Edit <code>src/App.js</code> and save to reload.
-                </p>
-            </header>
+            <section id="home">
+                <header className="App-header">
+                    <img src={logo} className="App-logo" alt="logo" />
+                    <p>
+                        Edit <code>src/App.js</code> and save to reload.
+                    </p>
+                </header>
+            </section>
+            <section id='about-us'>
+                <p>about us</p>
+            </section>
         </div>
     );
 }
