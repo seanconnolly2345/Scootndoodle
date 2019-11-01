b1e59ebc073a51f508890bdf0d3ebac16e974fef
Aaron Leopold <aaronleopold1221@gmail.com>
2019-11-01 13:50:51 -0400

    removed underline decoration from nav items

--- client/src/components/Navigation/Navigation.js
+++ client/src/components/Navigation/Navigation.js
@@ -9,10 +9,10 @@ export default class Navigation extends React.Component {
                 <Navbar.Toggle area-controls='basic-navbar-nav' />
                 <Navbar.Collapse id='basic-navbar-nav'>
                     <Nav>
-                        <NavLink className='d-inline p-2 bg-dark text-white' to='/'>Home</NavLink>
-                        <NavLink className='d-inline p-2 bg-dark text-white' to='/#about-us'>About Us</NavLink>
-                        <NavLink className='d-inline p-2 bg-dark text-white' to='/shop'>Buy</NavLink>
-                        <NavLink className='d-inline p-2 bg-dark text-white' to='/#contact-us'>Contact Us</NavLink>
+                        <NavLink className='d-inline p-2 bg-dark text-white' to='/' style={{ textDecoration: 'none' }}>Home</NavLink>
+                        <NavLink className='d-inline p-2 bg-dark text-white' to='/#about-us' style={{ textDecoration: 'none' }}>About Us</NavLink>
+                        <NavLink className='d-inline p-2 bg-dark text-white' to='/shop' style={{ textDecoration: 'none' }}>Buy</NavLink>
+                        <NavLink className='d-inline p-2 bg-dark text-white' to='/#contact-us' style={{ textDecoration: 'none' }}>Contact Us</NavLink>
                     </Nav>
                 </Navbar.Collapse>
             </Navbar>
