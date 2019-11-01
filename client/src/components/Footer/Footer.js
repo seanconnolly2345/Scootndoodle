9f6f986242a06f06ad40ac34bd560f65765323bf
Aaron Leopold <aaronleopold1221@gmail.com>
2019-11-01 13:16:50 -0400

    added basic footer component

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -1,12 +1,22 @@
 import React from 'react';
 //import { Link } from 'react-router-dom';
+import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row'
 import 'bootstrap/dist/css/bootstrap.min.css';
+import './Footer.css'
 
 const Footer = () => {
     return (
-        <div>
-            this will be the footer
-        </div>
+        <Container>
+            <div className='footer-container'>
+                <Row className="justify-content-center">
+                    this is the footer
+                </Row>
+                <Row className="justify-content-center">
+                    media links here
+                </Row>
+            </div>
+        </Container>
     )
 }
 
