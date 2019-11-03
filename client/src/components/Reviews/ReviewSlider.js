0eda562732cf77ff6469eba1d929a28b1baa076f
Aaron Leopold <aaronleopold1221@gmail.com>
2019-11-03 15:12:31 -0500

    added basic slider for reviews. ugly formatting

--- client/src/components/Reviews/ReviewSlider.js
+++ client/src/components/Reviews/ReviewSlider.js
@@ -1,10 +1,64 @@
 import React from 'react'
+import ReviewCard from './ReviewCard'
+import Slider from "react-slick";
+import Containter from 'react-bootstrap/Container'
 
-export const ReviewSlider = () => (
-    <React.Fragment>
-        there will be a review slider here
-    </React.Fragment>
-)
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
+//class SliderLeftArrow extends React.Component {
+//    render() {
+//        return (
+//            <a
+//                href="#"
+//                className="carousel__arrow carousel__arrow--left"
+//                onClick={this.props.onClick}
+//            >
+//            <span className="fa fa-2x fa-angle-left" />
+//            </a>
+//        )
+//    }
+//}
+
+//class SliderRightArrow extends React.Component {
+//    render() {
+//        return (
+//            <a
+//                href="#"
+//                className="carousel__arrow carousel__arrow--right"
+//                onClick={this.props.onClick}
+//            >
+//            <span className="fa fa-2x fa-angle-right" />
+//            </a>
+//        )
+//    }
+//}
+
+export default ({data}) => {
+    const reviewList = data.map(review => {
+        return <ReviewCard reviewData={review} />
+    })
+
+    const settings = {
+        autoplay: true,
+        dots: true,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 1,
+        slidesToScroll: 1
+    }
+
+    return (
+        <Containter>
+            <Slider {...settings}>
+                {reviewList}
+            </Slider>
+        </Containter>
+        
+    )
+}
+
+
 
 // https://codepen.io/Thunderboy/pen/LxjrgB
 // https://codepen.io/doreentseng/pen/VzbXRg
--- client/src/views/Home/Home.js
+++ client/src/views/Home/Home.js
@@ -3,13 +3,14 @@ import './Home.css';
 import { Video } from '../../components/Video/Video'
 import { About } from '../../components/About/About'
 import { Media } from '../../components/Media/Media';
-import { ReviewSlider } from '../../components/Reviews/ReviewSlider';
+import ReviewSlider from '../../components/Reviews/ReviewSlider';
+import sliderData from '../../assets/reviews'
 //import Container from 'react-bootstrap/Container'
 
 export const Home = () => (
     <React.Fragment>
         <Video />
-        <ReviewSlider />
+        <ReviewSlider data={sliderData}/>
         <About />
         <Media />
     </React.Fragment>
