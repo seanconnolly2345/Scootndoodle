d7918450f9de53eab962a171622945ef94581e9f
Aaron Leopold <aaronleopold1221@gmail.com>
2019-11-03 13:54:15 -0500

    added component for review modal

---
+++ client/src/components/Reviews/ReviewModal.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const ReviewModal = () => (
+    <React.Fragment>
+        there will be a review modal here
+    </React.Fragment>
+)
--- client/src/views/Home/Home.js
+++ client/src/views/Home/Home.js
@@ -3,11 +3,13 @@ import './Home.css';
 import { Video } from '../../components/Video/Video'
 import { About } from '../../components/About/About'
 import { Media } from '../../components/Media/Media';
+import { ReviewModal } from '../../components/Reviews/ReviewModal';
 //import Container from 'react-bootstrap/Container'
 
 export const Home = () => (
     <React.Fragment>
         <Video />
+        <ReviewModal />
         <About />
         <Media />
     </React.Fragment>
