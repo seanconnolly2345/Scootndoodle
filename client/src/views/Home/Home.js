1bbf60a34581d5a33c6c9b29470e290dff25aa46
Aaron Leopold <36278431+aaronleopold@users.noreply.github.com>
2019-11-11 18:42:53 -0500

    Revert "Front end"

--- client/src/views/Home/Home.js
+++ client/src/views/Home/Home.js
@@ -1,27 +1,25 @@
 import React from 'react';
+import logo from '../../assets/logo.svg';
 import './Home.css';
-import { Video } from '../../components/Video/Video'
-import { About } from '../../components/About/About'
-import Media from '../../components/Media/Media';
-import { ShopPreview } from '../../components/ShopPreview/ShopPreview'
-import ReviewSlider from '../../components/Reviews/ReviewSlider';
-import sliderData from '../../assets/reviews'
-import Footer from '../../components/Footer/Footer';
-import Navigation from '../../components/Navigation/Navigation';
-import Row from 'react-bootstrap/Row';
-import { MissionStatement } from '../../components/Mission/MissionStatement';
-//import Container from 'react-bootstrap/Container'
 
-export const Home = () => (
-    <React.Fragment>
-        <Navigation />
-        <Video />
-        <ReviewSlider data={sliderData}/>
-        <MissionStatement />
-        <ShopPreview />
-        <About />
-        <Media />
-    </React.Fragment>
-)
+function Home() {
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+                <p>
+                    Edit <code>src/App.js</code> and save to reload.
+                </p>
+                <a
+                    className="App-link"
+                    href="https://reactjs.org"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                </a>
+            </header>
+        </div>
+    );
+}
 
-// https://github.com/reactjs/react-modal
+export default Home;
--- client/src/views/Shop/Shop.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import Footer from '../../components/Footer/Footer'
-
-export const Shop = () => (
-    <React.Fragment>
-        This will be the shop
-    </React.Fragment>
-)
