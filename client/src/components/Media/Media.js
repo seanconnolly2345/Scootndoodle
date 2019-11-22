fa475a2d412b078b8077f5aae66a8de65e803c55
Aaron Leopold <aaronleopold1221@gmail.com>
2019-11-22 13:16:26 -0500

    formatting changes

--- client/src/components/Media/Media.js
+++ client/src/components/Media/Media.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
+import Container from 'react-bootstrap/Container'
 import { SocialIcon } from 'react-social-icons'
 import InstaGrid from './InstaGrid'
 import { FacebookWidget } from './FacebookWidget'
@@ -15,14 +16,15 @@ class Media extends React.Component {
         return (
             <div>
                 <div id="media" />
-                <Row>
+                <Container>
+                <Row className='justify-content-center'>
                     <Col xs={12} md={6}>
                         <Row style={{marginLeft: 0, marginRight: 0}}><h2>Contact Us</h2></Row>
                         <Row style={{marginLeft: 0, marginRight: 0}}>
                             <SocialIcon url='https://www.facebook.com/' className='icon-padding' />
                             <SocialIcon url='https://www.instagram.com/scootscribbledoodletest/' style={{marginLeft: '1rem'}} />
                         </Row>
-                        <Row className='justify-content-center' style={{marginLeft: 0, marginRight: 0}}>
+                        <Row style={{marginLeft: 0, marginRight: 0}}>
                             <ContactForm />
                         </Row>
                     </Col>
@@ -30,6 +32,7 @@ class Media extends React.Component {
                         <FacebookWidget />
                     </Col>
                 </Row>
+                </Container>
                 <Row>
                     <InstaGrid account="scootscribbledoodletest" numberOfMediaElements={6} />
                 </Row>
